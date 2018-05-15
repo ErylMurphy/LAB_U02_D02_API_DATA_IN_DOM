89a9152fcdd90078bdeef88a1b1cd2d769baa7b4
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-05-15 11:39:26 -0400

    Overhaul the lab:
     - Store state in variables, not in data attributes.
     - Make single-responsibility functions, similar to what they'll need to
    do for project 2
     - Don't show a user's password ;)
     - Limit API results to users from the US for more familiar data

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
-$(function(){
-  console.log('script loaded.')
+console.log("script loaded.");
 
-  // part 0.5 - url: https://open-source-champ-api.herokuapp.com/api/v1/players
+let currentPerson = {
+  name: "Eric Lewis",
+  email: "eric.andrew.lewis@gmail.com",
+  phone: "123 456 7890",
+  birthday: "1986-06-22 07:04:05",
+  address: "123 First Avenue, New York, NY",
+  image: "images/myself.jpg"
+};
 
+// Part 0 - visit  in your browser and take a glance
+// at the kind of data the Random User API provides.
 
-  // part 1 - make a function called 'makeCall' that will make an AJAX GET request to 'https://randomuser.me/api/' and console.log the response in the success function - 10 min
-  
+// Part 1 (10 minutes) - make a function called getRandomPerson.
+// - It should make an AJAX request to 'https://randomuser.me/api/?nat=us' with fetch.
+// - Add a function to the promise chain with .then() that resolves the response body as JSON.
+// - Add another function to the promise chain that logs the JSON object to the console.
+// - The function should return the promise.
 
-  // part 2 - write a function called getData that will parse the results from the ajax request and set new variables for the user's first name, last name, email, dob, street, city, state, address, phone, password, and image. Call this function from the success method of your AJAX call. - 15 min
+// Part 2 (15 minutes) - In the last callback on the Promise chain in getRandomPerson,
+// create a person object that extracts data from the JSON.
+//
+// The object should include the random person's name (first and last), email, phone,
+// birthday, address (street, city, and state), and image (use the large version).
+//
+// Return the person object in the callback function, so that whenever getRandomPerson()
+// is called, it will resolve with the format of data we are interested in.
 
+// Part 3 (10 minutes) - Write a click handler for the button that calls getRandomPerson.
+// Add a .then() callback on this invocation of getRandomPerson that logs
+// the person object to the console.
 
-  // part 3 - write a function called manipulateDom that will accept the name, email, dob, address, phone, pw, and image as arguments. Assign these values as data attributes to the html elements as follows.
-  // the element with the id of 'bigtext' should have the the user's name for its text
-  // the element with the id of 'photo' shouls have the user's image for it's background-image
-  // the element witht he id of 'name' should have the user's name for its data attribute
-  // the element with the id of email should have the user's email for its data attribute
-  // the element with the id of birthdate should have the user's birthdate for its data attribute
-  // the element with the id of phone should have the user's phone for its data attribute
-  // the element with the id of password should have the user's password for its data attribute
-  // HINT - CHECKOUT the jquery attr method
-  // call this function from inside getData and pass in the appropriate values - 20 min
+// Part 4 (20 minutes) - write a function updatePerson. It should:
+// - be called in the click handler's .then() callback function with the person object
+//   as an argument.
+// - update the element with the id of 'smalltext' to have the text 'My name is'
+// - update the element with the id of 'bigtext' to have the the person's name as its text
+// - update the element with the id of 'photo' to have the user's image as it's background-image
+// - update the outer scoped variable currentPerson to the value of person.
 
+// part 5 (25 minutes) - write a function called addEventListeners, which is called immediately.
+//
+// It will add hover event listeners for each of the icons on the page:
+// - when hovering over #name, the #smalltext should say 'My name is',
+//   and the #bigtext should show the currentPerson's name as its content
+//
+// - when hovering over #email, the #smalltext should say 'My email is',
+//   and the #bigtext should show the currentPerson's email as its content
+//
+// - when hovering over #birthdate, the #smalltext should say 'My birthday is',
+//   and the #bigtext should show the currentPerson's birthday as its content.
+//   The birthday should be formatted MM/DD/YYYY, e.g. 5/15/2018. Hint: use
+//   the JavaScript Date object and use date object methods https://www.w3schools.com/jsref/jsref_obj_date.asp
+//
+// - when hovering over #address, the #smalltext should say 'My addresss is',
+//   and the #bigtext should show the currentPerson's address as its content
+//
+// - when hovering over #phone, the #smalltext should say 'My phone number is',
+//   and the #bigtext should show the currentPerson's phone number as its content
 
-  // part 4 - write a click handler for the button that will call the makeCall function - 5 min
-
-
-  // part 5 - write a function called addEventListeners that will add hover listeners for the icons on the page - 20 min
-  //
-  // when hovering over #name, the #smalltext should say 'My name is', and the #bigtext should use the #name's data attribute for its text
-  // when hovering over #email, the #smalltext should say 'My email is', and the #bigtext should use the #email's data attribute for its text
-  // when hovering over #birthdate, the #smalltext should say 'My birthday is', and the #bigtext should use the #birthdate's data attribute for its text
-  // when hovering over #address, the #smalltext should say 'My addresss is', and the #bigtext should use the #address's data attribute for its text
-  // when hovering over #phone, the #smalltext should say 'My phone number is', and the #bigtext should use the #phone's data attribute for its text
-  // when hovering over #password, the #smalltext should say 'My password is', and the #bigtext should use the #password's data attribute for its text
-
-})
+// BONUS
