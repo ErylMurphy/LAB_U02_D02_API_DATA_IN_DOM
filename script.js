8bb22c1ca1de70dc447312ccf54719f47f46f64a
Eryl <eryl.murphy@gmail.com>
2018-05-15 16:28:39 -0400

    made magic

--- script.js
+++ script.js
@@ -17,7 +17,85 @@ let currentPerson = {
 // - Add a function to the promise chain with .then() that resolves the response body as JSON.
 // - Add another function to the promise chain that logs the JSON object to the console.
 // - The function should return the promise.
+const getRandomPerson = () => {
+  return fetch('https://randomuser.me/api/',)
+    .then(response => {
+      const responseConst = response.json();
+      return responseConst;
+      console.log(responseConst);
+  })
+    .then(json => {
+      console.log(json);  
+      const result = json.results[0];
+      const person = {
+        name: result.name.first + " " + result.name.last,
+        email: result.email,
+        phone: result.phone,
+        birthday: result.dob,
+        address: result.location.city + ", " + result.location.city + ", " + result.location.state, 
+        image: result.picture.large
 
+      };
+      return person; 
+  });}
+
+  // getRandomPerson().then (person => {
+  //   console.log(person);
+
+  // //   console.log(response.json.results[0]);
+  // //  return response.json.results[0];
+  // })
+
+$('button').click(function () {
+  getRandomPerson().then(person => {
+    console.log(person);
+})})
+ 
+const updatePerson = person => {
+  let smallText = $('#smalltext').html('My name be');
+  let bigText = $('#bigtext').html(person.name);
+  let photo = $('#photo').css('background-image', "url(" + person.image + ")");
+  currentPerson = person;
+};
+
+
+$('#smalltext').hover(function () {
+  $('#smalltext').html('My name is');
+});
+
+$('#email').hover(function () {
+  $('#smalltext').html('my email is');
+  $('#bigtext').html(currentPerson.email)
+  
+})
+
+
+
+// const addEventListeners = updatePerson => {
+//   smallText.hover('My name is');
+//   bigText.hover(currentPerson.name);
+//   console.log(smallText);
+//   console.log(bigText)
+// }
+// part 5 (25 minutes) - write a function called addEventListeners, which is called immediately.
+//
+// It will add hover event listeners for each of the icons on the page:
+// - when hovering over #name, the #smalltext should say 'My name is',
+//   and the #bigtext should show the currentPerson's name as its content
+//
+// - when hovering over #email, the #smalltext should say 'My email is',
+//   and the #bigtext should show the currentPerson's email as its content
+//
+// - when hovering over #birthdate, the #smalltext should say 'My birthday is',
+//   and the #bigtext should show the currentPerson's birthday as its content.
+//   The birthday should be formatted MM/DD/YYYY, e.g. 5/15/2018. Hint: use
+//   the JavaScript Date object and use date object methods https://www.w3schools.com/jsref/jsref_obj_date.asp
+//
+// - when hovering over #address, the #smalltext should say 'My addresss is',
+//   and the #bigtext should show the currentPerson's address as its content
+//
+// - when hovering over #phone, the #smalltext should say 'My phone number is',
+//   and the #bigtext should show the currentPerson's phone number as its content
 // Part 2 (15 minutes) - In the last callback on the Promise chain in getRandomPerson,
 // create a person object that extracts data from the JSON.
 //
@@ -39,24 +117,6 @@ let currentPerson = {
 // - update the element with the id of 'photo' to have the user's image as it's background-image
 // - update the outer scoped variable currentPerson to the value of person.
 
-// part 5 (25 minutes) - write a function called addEventListeners, which is called immediately.
-//
-// It will add hover event listeners for each of the icons on the page:
-// - when hovering over #name, the #smalltext should say 'My name is',
-//   and the #bigtext should show the currentPerson's name as its content
-//
-// - when hovering over #email, the #smalltext should say 'My email is',
-//   and the #bigtext should show the currentPerson's email as its content
-//
-// - when hovering over #birthdate, the #smalltext should say 'My birthday is',
-//   and the #bigtext should show the currentPerson's birthday as its content.
-//   The birthday should be formatted MM/DD/YYYY, e.g. 5/15/2018. Hint: use
-//   the JavaScript Date object and use date object methods https://www.w3schools.com/jsref/jsref_obj_date.asp
-//
-// - when hovering over #address, the #smalltext should say 'My addresss is',
-//   and the #bigtext should show the currentPerson's address as its content
-//
-// - when hovering over #phone, the #smalltext should say 'My phone number is',
-//   and the #bigtext should show the currentPerson's phone number as its content
+
 
 // BONUS
